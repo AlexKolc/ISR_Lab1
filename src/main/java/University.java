bf51bdbe2240eb84980c7b242828b39eafd9b2f9
AlexKolc <alex_kolc@inbox.ru>
2019-10-18 02:07:24 +0300

    Added new class

---
+++ src/main/java/University.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class University {
+    List<Timetable> timetables;
+
+    University() {
+        timetables = new ArrayList<Timetable>();
+    }
+
+
+}
