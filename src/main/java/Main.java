3329b6b97753ce0bb209699636d3cfe7fa8c0ca2
Sergeeva Julia <32176800+Emidiant@users.noreply.github.com>
2019-10-18 11:00:28 +0300

    Commit from Julia! We done it

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,5 +11,10 @@ public class Main {
         
         //System.out.println(timetable.getAllTimetable());
         System.out.println(timetable.getTimetableByName("Economics"));
+
+        System.out.println("Hello world?!");
+
+        System.out.println("we need help");
+
     }
 }
