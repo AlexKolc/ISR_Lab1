0b90189b6fbb97f430fee247277957c48dac02cc
Julia <emidiant1999@gmail.com>
2019-10-18 10:23:56 +0300

    Commit from other people

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,5 +11,7 @@ public class Main {
         
         //System.out.println(timetable.getAllTimetable());
         System.out.println(timetable.getTimetableByName("Economics"));
+
+        System.out.println("Hello World!!!!?!?!?!?");
     }
 }
